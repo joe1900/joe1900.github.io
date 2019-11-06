90f7347cdeaef2f054bcea9b6f8af9f7014d259f
JiYinghao <joe@hisens.cc>
2019-11-06 16:39:47 +0800

    医生PC端

---
+++ precache-manifest.4d78b740ca9635f6569f3baca7e08266.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1d72b27befccc664db",
+    "url": "/css/app.09dabb23.css"
+  },
+  {
+    "revision": "42f3a967bc38e830a306",
+    "url": "/css/chunk-144e959b.d866cff7.css"
+  },
+  {
+    "revision": "c7c887bb3288382e288b",
+    "url": "/css/chunk-1a9229d9.b6934e19.css"
+  },
+  {
+    "revision": "88626e5d3b06bec97728",
+    "url": "/css/chunk-vendors.dde3861b.css"
+  },
+  {
+    "revision": "8ce752b9a6035b8cfb1182f65e528106",
+    "url": "/img/20191028190529.8ce752b9.png"
+  },
+  {
+    "revision": "5c4d972ddf3373063ebb9b9b25ca2a50",
+    "url": "/img/global_default_head@3x.5c4d972d.png"
+  },
+  {
+    "revision": "6e16292ba0c5284e8a951a16597163e6",
+    "url": "/img/me_us_logo@3x.6e16292b.png"
+  },
+  {
+    "revision": "11a044e727a8cb4be8abf5ff669a2894",
+    "url": "/img/wei.11a044e7.svg"
+  },
+  {
+    "revision": "3c4c8828928d5959320e308ef1367825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1d72b27befccc664db",
+    "url": "/js/app.31372854.js"
+  },
+  {
+    "revision": "42f3a967bc38e830a306",
+    "url": "/js/chunk-144e959b.aaaaf6df.js"
+  },
+  {
+    "revision": "c7c887bb3288382e288b",
+    "url": "/js/chunk-1a9229d9.1e87c627.js"
+  },
+  {
+    "revision": "88626e5d3b06bec97728",
+    "url": "/js/chunk-vendors.eb178e36.js"
+  },
+  {
+    "revision": "89559606134f682891a77972e37faf9c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cd6b7a13e4b78c06838e15c43baada9d",
+    "url": "/voice.js"
+  }
+]);
